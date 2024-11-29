3b50faff0256953ba8e8fbdced7b48b41ff7ca97
darren <darren.kooyz@gmail.com>
2024-11-29 17:48:54 +0800

    fix: return testing

--- config/payment-gateway.php
+++ config/payment-gateway.php
@@ -2,14 +2,14 @@
 
 return [
     'staging' => [
-        'paymentUrl' => 'https://local7.currenttech.pro',
+        'paymentUrl' => 'https://ttpayment-gateway.currenttech.pro',
         'appId' => 'fxtrado',
         'merchantId' => '16',
         'ttKey' => 'eMVpNySWd9bLYXUD1lsf9aoZ9o0uBO9ixUjpPZLtSnchpM1CES',
         // 'language' => 'en',
     ],
     'live' => [
-        'paymentUrl' => 'https://securepay.ttbvnk.com/',
+        'paymentUrl' => 'https://securepay.ttbvnk.com',
         'appId' => 'fxtrado',
         'ttKey' => '5uBp33lSYYOUjiEhkFmU2yIApVO0GNlWl4JiCZAOL5mpNuMcyD',
         'merchantId' => '5'
